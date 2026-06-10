2f3c671833a48052491e9ef6375838bb6b22c1ad
22304 <22304@students.mrgs.school.nz>
2026-06-11 09:54:56 +1200

    committ

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ image2 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), si
 image2_button = customtkinter.CTkButton(menu_page_frame, text="Classic waffle", image=image2, command= classic_waffle )
 image2_button.grid(row=3, column=1)
 
-
+f
 
 
 app.mainloop()
