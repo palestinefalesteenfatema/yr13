0f08cd50a44008da828d3818888210650f8fd107
22304 <22304@students.mrgs.school.nz>
2026-07-24 15:04:44 +1200

    login page

--- main.py
+++ main.py
@@ -760,7 +760,6 @@ def Home():
     global bubble_waffle_frame
     global waffle_bites_frame
     global waffle_cake_frame
-    global waffle_fries_frame
     global iced_choclate_frame
     global smoothies_frame
     smoothies_frame.place_forget()
@@ -769,7 +768,6 @@ def Home():
     bubble_waffle_frame.place_forget()
     waffle_bites_frame.place_forget()
     waffle_cake_frame.place_forget()
-    waffle_fries_frame.place_forget()
     home_page_frame.place(x=1, y=1, relwidth=1, relheight=1)
     menu_page_frame.place_forget()
 def menu_bt():
@@ -779,7 +777,6 @@ def menu_bt():
      global bubble_waffle_frame
      global waffle_bites_frame
      global waffle_cake_frame
-     global waffle_fries_frame
      global iced_choclate_frame
      global smoothies_frame
      smoothies_frame.place_forget()
@@ -788,7 +785,6 @@ def menu_bt():
      bubble_waffle_frame.place_forget()
      waffle_bites_frame.place_forget()
      waffle_cake_frame.place_forget()
-     waffle_fries_frame.place_forget()
      home_page_frame.place_forget()
      menu_page_frame.place(x=1, y=1, relwidth=1, relheight=1)
 
@@ -811,7 +807,8 @@ def login_page_bt():
     waffle_fries_frame.place_forget()
     home_page_frame.place_forget()
     login_page_frame.place(x=1, y=1, relwidth=1, relheight=1)
-
+def confirm():
+    pass
 
 #home page frame
 home_page_frame = customtkinter.CTkScrollableFrame(app)
@@ -885,7 +882,7 @@ def bubble_waffle():
     print(price)
 
 bubble_waffle_frame = customtkinter.CTkScrollableFrame(app, width=750, border_width=2)
-image3 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
+image3 = customtkinter.CTkImage(light_image=Image.open("bubblewl.png"), size=(200,200))
 image3_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image3, command= bubble_waffle )
 image3_button.grid(row=3, column=2)
 #bananas
@@ -907,35 +904,7 @@ number_of_items_label = customtkinter.CTkLabel(bubble_waffle_frame, text= "Numbe
 number_of_items_label.grid(column=2, row=3, columnspan=3)
 number_of_items = Spinbox(bubble_waffle_frame, from_=1, to=1000000000,font=("Arial", 14), width=10)
 number_of_items.grid(column=2, row=4, columnspan=3)
-# waffle fries
-def waffle_fries():
-    global price
-    global waffle_fries
-    global menu_page_frame
-    waffle_fries_frame.place(relx=0.5, rely=0.3, anchor="center")
-    menu_page_frame.place_forget()
-    price = +20.65
-    print(price)
-
-
-waffle_fries_frame = customtkinter.CTkScrollableFrame(app, width=650, border_width=2)
-image2 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image2_button = customtkinter.CTkButton(menu_page_frame, image=image2, border_width= 1 ,text="",fg_color= "white",hover_color="lightgray",command= waffle_fries )
-image2_button.grid(row=3, column=3)
-#bananas
-banana_radio = customtkinter.StringVar()
-banana = customtkinter.CTkRadioButton(waffle_fries_frame, text= "Banana", variable=banana_radio).grid(row=0, column=1, padx=20, pady=20, sticky="ew", columnspan=1)
-no_banana = customtkinter.CTkRadioButton(waffle_fries_frame, text= "No Banana", variable=banana_radio).grid(row=0, column=2, padx=20, pady=20, sticky="e", columnspan=1)
-#ice cream
-whiped_cream_radio = customtkinter.StringVar()
-whiped_cream = customtkinter.CTkRadioButton(waffle_fries_frame, text= "Whiped cream", variable=icecream_radio).grid(row=1, column=1, padx=20, pady=20, columnspan=1)
-no_whiped_cream = customtkinter.CTkRadioButton(waffle_fries_frame, text= "No whiped cream", variable=icecream_radio).grid(row=1, column=2, padx=20, pady=20, columnspan=1)
-confirm_classic = customtkinter.CTkButton(waffle_fries_frame, text= "confirm").grid(row=5, column=3, padx=20, pady=20, columnspan=1)
-# number of items
-number_of_items_label = customtkinter.CTkLabel(waffle_fries_frame, text= "Number of items" )
-number_of_items_label.grid(column=2, row=3, columnspan=3)
-number_of_items = Spinbox(waffle_fries_frame, from_=1, to=1000000000,font=("Arial", 14), width=10)
-number_of_items.grid(column=2, row=4, columnspan=3)
+#
 #waffle bites
 def waffle_bites():
     global waffle_bites_frame
@@ -946,8 +915,8 @@ def waffle_bites():
     price = +20.65
     print(price)
 waffle_bites_frame = customtkinter.CTkScrollableFrame(app, width=750, border_width=2)
-image5 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image5_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image2, command= waffle_bites)
+image5 = customtkinter.CTkImage(light_image=Image.open("bites.png"), size=(200,200))
+image5_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image5, command= waffle_bites)
 image5_button.grid(row=4, column=1)
 
 #waffle cake
@@ -961,7 +930,7 @@ def waffle_cake():
     print(price)
 
 waffle_cake_frame = customtkinter.CTkScrollableFrame(app, width=750, border_width=2)
-image3 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
+image3 = customtkinter.CTkImage(light_image=Image.open("cake.png"), size=(200,200))
 image3_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image3, command= waffle_cake )
 image3_button.grid(row=4, column=2)
 #bananas
@@ -974,9 +943,9 @@ icecream = customtkinter.CTkRadioButton(waffle_cake_frame, text= "Ice cream", va
 no_icecream2 = customtkinter.CTkRadioButton(waffle_cake_frame, text= "No ice cream", variable=icecream_radio).grid(row=1, column=2, padx=20, pady=20, columnspan=1)
 double_icecream = customtkinter.CTkRadioButton(waffle_cake_frame, text= "Double scoop", variable=icecream_radio).grid(row=1, column=3, padx=20, pady=20, columnspan=1)
 confirm_classic = customtkinter.CTkButton(waffle_cake_frame, text= "confirm").grid(row=5, column=3, padx=20, pady=20, columnspan=1)
-number_of_items_label = customtkinter.CTkLabel(waffle_fries_frame, text= "Number of items" )
+number_of_items_label = customtkinter.CTkLabel(waffle_cake_frame, text= "Number of items" )
 number_of_items_label.grid(column=2, row=3, columnspan=3)
-number_of_items = Spinbox(waffle_fries_frame, from_=1, to=1000000000,font=("Arial", 14), width=10)
+number_of_items = Spinbox(waffle_cake_frame, from_=1, to=1000000000,font=("Arial", 14), width=10)
 number_of_items.grid(column=2, row=4, columnspan=3)
 
 
@@ -994,8 +963,8 @@ def iced_choclate():
 
 
 iced_choclate_frame = customtkinter.CTkScrollableFrame(app, width=750, border_width=2)
-image9 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image9_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image3, command= iced_choclate)
+image9 = customtkinter.CTkImage(light_image=Image.open("chocolate.png"), size=(200,200))
+image9_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image9, command= iced_choclate)
 image9_button.grid(row=6, column=1)
 #size
 size_radio = customtkinter.StringVar()
@@ -1018,8 +987,8 @@ def smoothies():
     menu_page_frame.place_forget()
     smoothies_frame.place(relx=0.5, rely=0.3, anchor="center")
 smoothies_frame = customtkinter.CTkScrollableFrame(app, width=750, border_width=2)
-image10 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image10_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image2, command= smoothies )
+image10 = customtkinter.CTkImage(light_image=Image.open("smoothies.png"), size=(200,200))
+image10_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image10, command= smoothies )
 image10_button.grid(row=6, column=3)
 #flavours
 flavours_label = customtkinter.CTkLabel(smoothies_frame, text= "flavours" )
@@ -1049,83 +1018,14 @@ def slushies():
     global slushies_label
     slushies_label.grid(row=3, column=3)
 slushies_label = customtkinter.CTkLabel(menu_page_frame, text="slushies")
-image11 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image11_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image2, command= slushies )
+image11 = customtkinter.CTkImage(light_image=Image.open("slushies.png"), size=(200,200))
+image11_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray",command= slushies, image= image11 )
 image11_button.grid(row=7, column=1)
 
-hot_drinks_lable = customtkinter.CTkLabel(menu_page_frame, text ="hot_drinks").grid(row=8, column=2)
-
-#Coffee
-def coffee():
-    global coffee_label
-    coffee_label.grid(row=4, column=4)
-coffee_label = customtkinter.CTkLabel(menu_page_frame, text="Coffee")
-image7 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image7_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image2, command= coffee )
-image7_button.grid(row=9, column=1)
-#hot Choclate
-
-def hot_choclate():
-    global choclate_drink_label
-    choclate_drink_label.grid(row=3, column=3)
-choclate_drink_label = customtkinter.CTkLabel(menu_page_frame, text="hot_choclate")
-image9 = customtkinter.CTkImage(light_image=Image.open("classic waffle.png"), size=(200,200))
-image9_button = customtkinter.CTkButton(menu_page_frame, text="",fg_color= "white",hover_color="lightgray", image=image2, command= hot_choclate )
-image9_button.grid(row=9, column=2)
-
-
-# login page
-
-def is_valid_email(email):
-    pattern=r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    return re.match(pattern,email)
-
-
-def signup():
-    name=name_entry.get()
-    email=email_entry.get()
-    password=password_entry.get()
-    if not name or not email or not password:
-        return
-    if not is_valid_email(email):
-        messagebox.showerror("Invalid email")
-        return
-    if not os.path.exists(infobase):
-        wb = Workbook()
-        ws = wb.active
-        ws.append(['Name', 'Email', 'Password'])
-        wb.save(infobase)
-
-    wb=load_workbook(infobase)
-    ws=wb.active
-    ws.append([name,email,password])
-    wb.save(infobase)
-    wb.close()
-    messagebox.showinfo('signup Succesfull')
 
 
 
-infobase=[
-    {
-    "name": "layla",
-    "email":"22304@students.mrgs.school.nz",
-    "password":"password"
-    },
-    {
-    "name": "layla",
-    "email":"22304@students.mrgs.school.nz",
-    "password":"password"
-    }
-]
 
-#name
-name_entry = CTkEntry(login_page_frame, placeholder_text= "name", width=250).grid(column=0, row=0, columnspan=3)
-#email
-email_entry = CTkEntry(login_page_frame, placeholder_text= "email", width=250).grid(column=0, row=1, columnspan=3)
-#password
-password_entry = CTkEntry(login_page_frame, placeholder_text= "password", width=250,show='*').grid(column=0, row=2, columnspan=3)
-
-signup_button= CTkButton(login_page_frame, text="Sign Up", width=250, command=signup).grid(column=0, row=3, columnspan=3)
 
 
 
@@ -1269,64 +1169,6 @@ topping8.grid(row=22, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 
 topping8 = customtkinter.CTkCheckBox(bubble_waffle_frame, text= "chopped up Whittaker's Creamy Milk")
 topping8.grid(row=23, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-#fries
-Toppings_label = customtkinter.CTkLabel(waffle_fries_frame, text="Toppings").grid(row=0, column=0)
-topping1 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Maple")
-topping1.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping2 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Lotus Biscoff Sauce")
-topping2.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping3 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Strawberry")
-topping3.grid(row=4, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping4 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Passion Fruit")
-topping4.grid(row=5, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping5 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Chocolate Sauce ")
-topping5.grid(row=6, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping6 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Peanut Butter sauce")
-topping6.grid(row=7, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping7= customtkinter.CTkCheckBox(waffle_fries_frame, text= "Fruit silk Sauce")
-topping7.grid(row=8, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping8 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Citrus Peel topping")
-topping8.grid(row=9, column=0, padx=20, pady=20, sticky="ew", columnspan=1)
-
-topping9 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Extra Ice Cream Scoop")
-topping9.grid(row=10, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping10 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Lotus Biscoff Sauce")
-topping10.grid(row=11, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping11 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Pistachio topping sauce")
-topping11.grid(row=12, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping12 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Almond topping sauce")
-topping12.grid(row=13, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping13 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Dulce de Leche Caramel")
-topping13.grid(row=14, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping14 = customtkinter.CTkCheckBox(waffle_fries_frame, text= " hazelnut spread")
-topping14.grid(row=15, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping15 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Extra Banana")
-topping15.grid(row=16, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping16 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Fruit silk Sauce")
-topping16.grid(row=17, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping17 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Peanut Butter sauce")
-topping17.grid(row=18, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping16 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "Add Lotus Biscoff crumbles ")
-topping16.grid(row=19, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
-
-topping16 = customtkinter.CTkCheckBox(waffle_fries_frame, text= "chopped up Whittaker's PEANUT SLAB ")
-topping16.grid(row=20, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 #bites
 Toppings_label = customtkinter.CTkLabel(waffle_cake_frame, text="Toppings").grid(row=0, column=0)
 topping1 = customtkinter.CTkCheckBox(waffle_cake_frame, text= "Maple")
